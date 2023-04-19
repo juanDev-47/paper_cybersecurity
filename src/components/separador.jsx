0aac677c9ef1353606ae290980373a473b792a9c
juanDev-47 <juanp.arenas@udea.edu.co>
2023-04-18 20:43:25 -0500

    update: change app.css

--- src/components/separador.jsx
+++ src/components/separador.jsx
@@ -2,10 +2,10 @@ const Separador = (props) => {
 
   const { titulo } = props;
   return (
-    <div className='container-separador w-4/5 mx-auto py-5'>
+    <div className='container-separador mx-auto py-5'>
       <p className='my-6 py-1 text-center font-bold text-xl'>{titulo}</p>
     </div>
-    
+
   );
 };
 
