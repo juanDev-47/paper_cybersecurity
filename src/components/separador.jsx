a61636289141902f8686c2f1a903009f79af59cc
juanDev-47 <juanp.arenas@udea.edu.co>
2023-04-18 20:38:40 -0500

    update: test

--- src/components/separador.jsx
+++ src/components/separador.jsx
@@ -5,6 +5,7 @@ const Separador = (props) => {
     <div className='container-separador w-4/5 mx-auto py-5'>
       <p className='my-6 py-1 text-center font-bold text-xl'>{titulo}</p>
     </div>
+    
   );
 };
 
