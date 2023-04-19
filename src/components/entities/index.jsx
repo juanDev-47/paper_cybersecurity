87f26da88aa707f3582ecdee9bbc392015d459f6
Diego Alejandro Poveda <alejopov@gmail.com>
2023-04-19 16:57:39 -0500

    hide image if screen size is too small

--- src/components/entities/index.jsx
+++ src/components/entities/index.jsx
@@ -7,7 +7,7 @@ const Entities = () => {
   const items = [t('entity1'), t('entity2')];
 
   return (
-    <div className='w-3/4 mx-auto mb-4'>
+    <div className='w-3/4 max-w-[1200px] mx-auto mb-4'>
       <Separador titulo={t('entities')} />
       <div className='flex justify-center items-center gap-3'>
         <div>
@@ -19,7 +19,7 @@ const Entities = () => {
             ))}
           </ul>
         </div>
-        <div>
+        <div className='hidden md:block'>
           <img
             src={EntitiesImg}
             alt='Imagen entes'
