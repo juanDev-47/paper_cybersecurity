1171032ba8af7a31e33e0349e237db3ec0534ee9
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-05-19 13:51:45 -0500

    fix bug

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import ItemsContainer from "./itemsContainer";
+import React from 'react';
+import ItemsContainer from './itemsContainer';
 
 const Footer = () => {
-    return <footer className="bg-main text-white">
+    return <footer className='bg-main text-white'>
         <ItemsContainer />
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-10 text-center align-text-bottom pt-2 text-gray-300 text-sm pb-3  bg-sky-950">
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-10 text-center align-text-bottom pt-2 text-gray-300 text-sm pb-3  bg-sky-950'>
             <span className='my-4'>@2023 Workbots. All rights reserved</span>
         </div>
     </footer>
-
-        ;
 };
 
 export default Footer;
