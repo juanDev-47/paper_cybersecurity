78fed840c794a0be1e18f84aa7f4f59c3b7eb144
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-05-19 23:50:01 -0500

    commented unsed variables

--- src/pages/Methodology/Method.js
+++ src/pages/Methodology/Method.js
@@ -1,18 +1,18 @@
-import { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useContextProvider } from '../../context/contextProvider';
-import i18n from '../../i18n';
+// import { useEffect } from 'react';
+// import { useTranslation } from 'react-i18next';
+// import { useContextProvider } from '../../context/contextProvider';
+// import i18n from '../../i18n';
 
 const Method = () => {
-    const { traslate } = useContextProvider();
-    const { t } = useTranslation();
-    useEffect(() => {
-      if (traslate) {
-        i18n.changeLanguage('en');
-      } else {
-        i18n.changeLanguage('es');
-      }
-    }, [traslate]);
+    // const { traslate } = useContextProvider();
+    // const { t } = useTranslation();
+    // useEffect(() => {
+    //   if (traslate) {
+    //     i18n.changeLanguage('en');
+    //   } else {
+    //     i18n.changeLanguage('es');
+    //   }
+    // }, [traslate]);
 
     return <div>
         method
