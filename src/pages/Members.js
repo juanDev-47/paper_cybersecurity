6f36e5bc6aab9f342882ec3059941a0527118d26
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-05-20 11:10:06 -0500

    added members information

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -3,6 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { useContextProvider } from '../context/contextProvider';
 import i18n from '../i18n';
 import Nombres from '../components/nombres';
+import image1 from '../images/profile/Tatiana Sánchez Sanin.jpg'
+import image2 from '../images/profile/Juan Pablo Arenas.jpg'
+import image3 from '../images/profile/Diego Alejandro Poveda.jpg'
+import image4 from '../images/profile/Yohel Perez.jpg'
+//import image5 from '../images/profile/'
 
 const Members = () => {
   const { traslate } = useContextProvider();
@@ -14,51 +19,56 @@ const Members = () => {
       i18n.changeLanguage('es');
     }
   }, [traslate]);
-  return <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-20 object-center'>
+  return <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-20 justify-items-center'>
     <Nombres
       name='Tatiana E. Sánchez Sanin'
+      image={image1}
       text1={t('text1')}
       text2={t('text2')}
       text3={t('text3')}
       email='tatiana.sanchez1@udea.edu.co'
-      enlace1 = 'https://github.com/TatianaSanchez01'
-      enlace2 = 'https://www.linkedin.com/in/tatiana-sanchez-sanin/'
+      enlace1='https://github.com/TatianaSanchez01'
+      enlace2='https://www.linkedin.com/in/tatiana-sanchez-sanin/'
     />
     <Nombres
       name='Juan P. Arenas Velez'
+      image={image2}
       text1={t('text1')}
       text2={t('text2')}
       text3={t('text3')}
       email='juanp.arenas@udea.edu.co'
-      enlace1 = 'https://github.com/juanDev-47'
-      enlace2 = 'https://www.linkedin.com/in/juandev47/'
+      enlace1='https://github.com/juanDev-47'
+      enlace2='https://www.linkedin.com/in/juandev47/'
     />
     <Nombres
       name='Diego A. Poveda Alzate'
+      image={image3}
       text1={t('text1')}
       text2={t('text2')}
       text3={t('text3')}
       email='diego.poveda1@udea.edu.co'
-      enlace1 = 'https://github.com/diegopovalz'
-      enlace2 = 'https://www.linkedin.com/in/diegopovalz/'
+      enlace1='https://github.com/diegopovalz'
+      enlace2='https://www.linkedin.com/in/diegopovalz/'
     />
     <Nombres
       name='Yohel O. Pérez García'
+      image={image4}
       text1={t('text1')}
       text2={t('text2')}
       text3={t('text3')}
       email='yohel.perez@udea.edu.co'
-      enlace1 = 'https://github.com/yohelperez'
-      enlace2 = 'https://www.linkedin.com/in/yohel-p%C3%A9rez-0b05551a9/'
+      enlace1='https://github.com/yohelperez'
+      enlace2='https://www.linkedin.com/in/yohel-p%C3%A9rez-0b05551a9/'
     />
     <Nombres
       name='Johan S. Muentes Casadiego'
+      image=''
       text1={t('text1')}
       text2={t('text2')}
       text3={t('text3')}
       email='johan.muentes@udea.edu.co'
-      enlace1 = ''
-      enlace2 = ''
+      enlace1=''
+      enlace2=''
     />
   </div>;
 };
