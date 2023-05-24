7b815fd66ff46094196b7cf977308ecf6b0be9b8
Diego Alejandro Poveda <alejopov@gmail.com>
2023-05-23 19:47:10 -0500

    add translation for navbar

--- src/components/footer/itemsContainer.jsx
+++ src/components/footer/itemsContainer.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
-import { links } from '../navbar/myLinks'
-
+import { linksES as links } from '../navbar/myLinks';
 
 const ItemsContainer = () => {
-    return (
-        <div className='grid grid-cols-1 sm:grid-cols-3 gap-4 w-full py-16 justify-items-center'>
-            {links.map((Link) => (
-                <div>
-                    <h1 className='mb-1 font-semibold uppercase text-xl'>{Link.name}</h1>
-                    <div >
-                        {Link.submenu &&
-                            <div>
-                                {Link.sublinks.map((sublinks, idx1) => (
-                                    <div key={idx1}>
-                                        <ul>
-                                            {sublinks.sublinks.map((mySublinks, idx2) => (
-                                                <li key={idx2}>
-                                                    <a href={mySublinks.link} className='text-gray-400 hover:text-teal-400 text-sm duration-300 cursor-pointer leading-6'>{mySublinks.name}</a>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                ))}
-                            </div>
-                        }
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-3 gap-4 w-full py-16 justify-items-center'>
+      {links.map((Link) => (
+        <div>
+          <h1 className='mb-1 font-semibold uppercase text-xl'>{Link.name}</h1>
+          <div>
+            {Link.submenu && (
+              <div>
+                {Link.sublinks.map((sublinks, idx1) => (
+                  <div key={idx1}>
+                    <ul>
+                      {sublinks.sublinks.map((mySublinks, idx2) => (
+                        <li key={idx2}>
+                          <a
+                            href={mySublinks.link}
+                            className='text-gray-400 hover:text-teal-400 text-sm duration-300 cursor-pointer leading-6'
+                          >
+                            {mySublinks.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 };
 
 export default ItemsContainer;
--- src/components/navbar/myLinks.jsx
+++ src/components/navbar/myLinks.jsx
@@ -1,38 +1,45 @@
-export const links = [
+export const linksES = [
   {
-    name: 'Planteamiento', submenu: true, sublinks: [
+    name: 'Planteamiento',
+    submenu: true,
+    sublinks: [
       {
         sublinks: [
-          { name: 'Objetivos', link: "/objectives" },
-          { name: 'Contexto', link: "/context" },
-          { name: 'Entes', link: "/entities" },
-          { name: 'Definiciones', link: "/definitions" },
-          { name: 'Necesidad', link: "/necessity" },
-          { name: 'Problema', link: "/problem" }
-        ]
-      }
-    ]
-  }, {
-    name: 'Metodología', submenu: true, sublinks: [
-      {
-        sublinks: [
-          { name: 'Descripción Metodológica', link: "/methodology" },
-          { name: 'Métodos', link: "/method" },
-          { name: 'Técnicas', link: "/technique" },
-          { name: 'Instrumentos', link: "/instrument" },
-          { name: 'Procedimiento', link: "/procedure" },
-        ]
-      }
-    ]
+          { name: 'Objetivos', link: '/objectives' },
+          { name: 'Contexto', link: '/context' },
+          { name: 'Entes', link: '/entities' },
+          { name: 'Definiciones', link: '/definitions' },
+          { name: 'Necesidad', link: '/necessity' },
+          { name: 'Problema', link: '/problem' },
+        ],
+      },
+    ],
   },
   {
-    name: 'Resultados', submenu: true, sublinks: [
+    name: 'Metodología',
+    submenu: true,
+    sublinks: [
       {
         sublinks: [
-          { name: 'Hallazgos', link: "/findings" },
-          { name: 'Análisis', link: "/analysis" }
-        ]
-      }
-    ]
-  }
-]
+          { name: 'Descripción Metodológica', link: '/methodology' },
+          { name: 'Métodos', link: '/method' },
+          { name: 'Técnicas', link: '/technique' },
+          { name: 'Instrumentos', link: '/instrument' },
+          { name: 'Procedimiento', link: '/procedure' },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Resultados',
+    submenu: true,
+    sublinks: [
+      {
+        sublinks: [
+          { name: 'Hallazgos', link: '/findings' },
+          { name: 'Análisis', link: '/analysis' },
+        ],
+      },
+    ],
+  },
+];
